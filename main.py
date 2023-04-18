20dd26b43af48f3cd55145300b42fde0e1b32a7c
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-18 11:57:32 +0530

    pr

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
@@ -13,29 +15,32 @@ print(df.isnull().sum())
 # Replace missing values with the mean
 df.fillna(df.mean(), inplace=True)
 
-# # Check for outliers
-# # You can use box plots or scatter plots to identify outliers
-# # Remove outliers that are more than 3 standard deviations away from the mean
+# Check for outliers
 df = df[(df['Price'] - df['Price'].mean()).abs() < 3 * df['Price'].std()]
 
-# # Check for correlations
+# Check for correlations
 corr_matrix = df.corr()
 print(corr_matrix['Price'].sort_values(ascending=False))
 
- # Normalize the data using standard scaler
-scaler = StandardScaler()
-X = scaler.fit_transform(df.drop({'Price','Product_id'}, axis=1))
-y = df['Price']
-
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['Price', 'Product_id'], axis=1), df['Price'], test_size=0.2, random_state=42)
 
-# Train a linear regression model on the training set
+# Normalize the data using standard scaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Transform the features to polynomial features
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_train_poly = poly_features.fit_transform(X_train_scaled)
+X_test_poly = poly_features.transform(X_test_scaled)
+
+# Train a polynomial regression model on the training set
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train_poly, y_train)
 
 # Evaluate the model on the training set
-y_pred = model.predict(X_train)
+y_pred = model.predict(X_train_poly)
 mse = mean_squared_error(y_train, y_pred)
 
 # Print the MSE and model parameters
